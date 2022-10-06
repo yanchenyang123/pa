189875afb806a6e2a973b1b2722b608e96f80c1c
tracer-ics2022 <tracer@njuics.org>
2022-10-06 19:55:28 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:28 up  2:40,  1 user,  load average: 0.11, 0.12, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -143,7 +143,6 @@ static bool make_token(char *e) {
     }
 
   }
-  printf("%d",nr_token);
 
   return true;
 }
@@ -286,6 +285,7 @@ paddr_t expr(char *e, bool *success)
     return 0;
   }
   int p=0;
+  printf("%d",nr_token);
   int q=nr_token-1;
   /* TODO: Insert codes to evaluate the expression. */
 
