65965f68855236d2bd16d7bc2ec74433b8148281
tracer-ics2022 <tracer@njuics.org>
2022-10-07 21:37:59 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:59 up  2:38,  1 user,  load average: 0.46, 0.53, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -151,6 +151,7 @@ static bool make_token(char *e) {
           tokens[nr_token].type=rules[i].token_type;
           substr(substr_start,substr_len);
           nr_token+=1;
+          printf("%s",tokens[nr_token].str);
           break;
       
 
