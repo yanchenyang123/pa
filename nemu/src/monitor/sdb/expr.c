e2f22ffc91986302a6093908d9ab1f21cb895925
tracer-ics2022 <tracer@njuics.org>
2022-10-07 21:44:11 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:44:11 up  2:44,  1 user,  load average: 0.18, 0.28, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -137,7 +137,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-      /*if(rules[i].token_type==TK_Reg)
+      if(rules[i].token_type==TK_Reg)
         {
           tokens[nr_token].type=rules[i].token_type;
           substr(substr_start,substr_len);
@@ -145,15 +145,14 @@ static bool make_token(char *e) {
           nr_token+=1;
 
           break;
-        }*/
-      
-      
+        }
+      else
+      {
           tokens[nr_token].type=rules[i].token_type;
           substr(substr_start,substr_len);
           nr_token+=1;
-          printf("%s",tokens[nr_token].str);
-          break;
-      
+      break;
+      }
 
         }
 
@@ -310,6 +309,7 @@ paddr_t f(int p,int q)
             };
             case TK_Reg:
             {
+              printf("1\n");
               return isa_reg_str2val(tokens[p].str,NULL);
             };
             case TK_num:
