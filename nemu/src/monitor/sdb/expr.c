f65b2163cafc7a092a83cd3131d2705992243256
tracer-ics2022 <tracer@njuics.org>
2022-10-08 21:43:29 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:29 up 12 min,  1 user,  load average: 0.16, 0.36, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -361,7 +361,7 @@ paddr_t f(int p,int q)
             case TK_YU: return val1&&val2;
             case TK_EQ: return val1==val2;
             case TK_NQ: return val1 != val2;
-            case TK_DEREF: return paddr_read(val1,4);
+            case TK_DEREF: return paddr_read(val2,4);
             default:assert(0);
             }
         
